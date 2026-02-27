9be3b02a44a0b873118383e017da2e55548d16ba
anishitha4-alt <anishitha4@gmail.com>
2026-02-27 16:13:59 +0530

    UC3 Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,36 +3,29 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
     /**
      * Application entry point
-     *
+     * <p>
      * This is a Java Palindrome Checker App Welcome Message
      *
      * @author Developer
      * @Version 1.0
      */
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input text:");
-        String input=sc.nextLine();
-        boolean isPalindrome;
-        for(int i=0;i<input.length()/2;i++){
-            if (input.charAt(i) != input.charAt((input.length()) - 1 - i)) {
-                 isPalindrome=false;
-                 break;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to check if it is a palindrome:");
+        String inputString = scanner.nextLine();
+        scanner.close();
 
-            }
-            else {
-                isPalindrome=true;
-                break;
+        String reverseString = "";
+        int length = inputString.length();
 
-            }
-
+         for (int i = length - 1; i >= 0; i--) {
+            reverseString = reverseString + inputString.charAt(i);
         }
-        if(isPalindrome=true){
-            System.out.println("Is it a Palindrome? :true");
 
-        }
-        else{
-            System.out.println("Is it a Palindrome? :false");
+         if (inputString.equals(reverseString)) {
+            System.out.println("Input string is a palindrome.");
+        } else {
+            System.out.println("Input string is not a palindrome.");
         }
 
     }
