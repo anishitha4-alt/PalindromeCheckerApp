0831573ce53de7396fae741941ab3228b557d005
anishitha4-alt <anishitha4@gmail.com>
2026-03-09 14:53:49 +0530

    UC11

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,32 +1,39 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    /**
-     * Application entry point
-     * <p>
-     * This is a Java Palindrome Checker App Welcome Message
-     *
-     * @author Developer
-     * @Version 1.0
-     */
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string to check if it is a palindrome:");
+        System.out.println("Input:");
         String inputString = scanner.nextLine();
         scanner.close();
 
-        String reverseString = "";
-        int length = inputString.length();
+        PalindromeService service = new PalindromeService();
 
-         for (int i = length - 1; i >= 0; i--) {
-            reverseString = reverseString + inputString.charAt(i);
-        }
+        boolean result = service.checkPalindrome(inputString);
 
-         if (inputString.equals(reverseString)) {
-            System.out.println("Input string is a palindrome.");
+        if(result){
+            System.out.println("Palindrome");
         } else {
-            System.out.println("Input string is not a palindrome.");
+            System.out.println("Not Palindrome");
+        }
+    }
+}
+
+class PalindromeService{
+    public boolean checkPalindrome(String input){
+        int start = 0;
+        int end = input.length() - 1;
+
+        while(start < end){
+            if(input.charAt(start) != input.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
         }
 
+        return true;
     }
+
 }
